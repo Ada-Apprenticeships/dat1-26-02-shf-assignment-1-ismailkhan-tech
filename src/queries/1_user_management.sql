af7582f46b56c7968052e7570ab68d0c91a71619
ismailkhan-tech <ismail.khan@ada.ac.uk>
2026-02-28 21:27:41 +0000

    started queries

--- src/queries/1_user_management.sql
+++ src/queries/1_user_management.sql
@@ -2,20 +2,52 @@
 .mode column
 
 -- 1.1
-SELECT * 
-FROM locations;
+SELECT 
+    member_id,
+    first_name,
+    last_name,
+    email,
+    join_date
+FROM members;
 
 
 -- 1.2
+UPDATE members
+SET
+
+    phone_number = '07000 100005',
+    email        = 'emily.jones.updated@email.com'
+WHERE member_id  = 5;
 
 
 -- 1.3
+SELECT COUNT(*) AS total_members
+FROM members;
 
 
 -- 1.4
-
+SELECT
+    m.member_id,
+    m.first_name,
+    m.last_name,
+    COUNT(ca.class_attendance_id) AS registration_count
+FROM members m
+JOIN class_attendance ca ON m.member_id = ca.member_id
+GROUP BY m.member_id
+ORDER BY registration_count DESC
+LIMIT 1;
 
 -- 1.5
+SELECT
+    m.member_id,
+    m.first_name,
+    m.last_name,
+    COUNT(ca.class_attendance_id) AS registration_count
+FROM members m
+JOIN class_attendance ca ON m.member_id = ca.member_id
+GROUP BY m.member_id
+ORDER BY registration_count ASC
+LIMIT 1;
 
 
 -- 1.6
